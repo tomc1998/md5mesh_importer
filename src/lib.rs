e31160c0e733203b410aafcdb5560709d463be8e
Thomas Cheng <thomascheng1998@googlemail.com>
2017-06-13 15:00:27 +0100

    Added verts, tris and weights parsing. Currently parsing the whole scene, tested on 1 md5 file.

--- src/lib.rs
+++ src/lib.rs
@@ -50,26 +50,69 @@ impl Joint {
 }
 
 /// A vertex in the MD5 file.
+#[derive(Debug, Clone)]
 pub struct Vert {
+  /// Index of this vertex (for indexed rendering)
+  index: usize,
+  /// Texture coords
+  uv: [f32; 2],
+  /// Index of the first vertex weight
+  weight_ix_start: usize,
+  /// Number of weights applied to this vertex
+  weight_ix_count: usize,
 }
 
+/// A tri in the MD5 file.
+#[derive(Debug, Clone)]
+pub struct Tri {
+  /// Index of this tri
+  index: usize,
+  /// The first vert of the tri
+  vert_index_1: usize,
+  /// The second vert of the tri
+  vert_index_2: usize,
+  /// The third vert of the tri
+  vert_index_3: usize,
+}
+
+/// A weight in the MD5 file.
+#[derive(Debug, Clone)]
+pub struct Weight {
+  /// Index of this weight
+  index: usize,
+  /// The joint associated with this weight
+  joint: usize,
+  /// The bias of the weight (scale between 0.0 and 1.0, how much this weight
+  /// contributes to the final vertex position)
+  bias: f32,
+  /// The position of this weight
+  pos: [f32; 3],
+}
+
+#[derive(Debug, Clone)]
 pub struct MD5Mesh {
+  /// Name of the material
+  shader: String,
+  /// List of this mesh's vertices
+  verts: Vec<Vert>,
+  /// List of this mesh's vertices
+  tris: Vec<Tri>,
+  /// List of this mesh's weights
+  weights: Vec<Weight>,
 }
 
 #[derive(Debug, Clone)]
 pub struct MD5Scene {
   pub header: MD5MeshHeader,
   pub joint_list: Vec<Joint>,
-  //pub shader_name: String,
-  //pub verts: Vec<Vert>
+  pub mesh_list: Vec<MD5Mesh>,
 }
 impl MD5Scene {
-  pub fn new(header: MD5MeshHeader, joint_list: Vec<Joint>) -> MD5Scene {
+  pub fn new(header: MD5MeshHeader, joint_list: Vec<Joint>, mesh_list: Vec<MD5Mesh>) -> MD5Scene {
     MD5Scene { 
       header: header,
       joint_list: joint_list,
-      //shader_name: "".to_owned(),
-      //verts: Vec::new(),
+      mesh_list: mesh_list,
     }
   }
 }
@@ -162,6 +205,18 @@ named!(parse_vec3f<&str, [f32; 3]>,
      )
   );
 
+/// Parser for a 2 dimension vector of f32s (UV coords).
+named!(parse_vec2f<&str, [f32; 2]>,
+       do_parse!(
+         opt!(space) >>
+         x: parse_f32 >>
+         space >>
+         y: parse_f32 >>
+         opt!(space) >>
+         ([x, y])
+     )
+  );
+
 /// Parser for md5mesh joints section
 named!(
   parse_md5mesh_joints<&str, Vec<Joint> >,
@@ -186,12 +241,165 @@ named!(
       )
     ) >>
     // Parse closing }
-    //tag!("}") >>
+    tag!("}") >>
     opt!(alt!(tag!("\n") | tag!("\r\n") | tag!("\r"))) >>
     (joint_vec)
   )
 );
 
+/// Consumes a line and parses a vert
+named!(parse_md5mesh_mesh_vert<&str, Vert>,
+  do_parse!(
+    space >> 
+    tag!("vert") >>
+    space >> 
+    vert_index: parse_u32 >>
+    space >>
+    uv: delimited!(tag!("("), parse_vec2f, tag!(")")) >>
+    space >>
+    start_weight: parse_u32 >> 
+    space >>
+    weight_count: parse_u32 >> 
+    opt!(alt!(tag!("\n") | tag!("\r\n") | tag!("\r"))) >>
+    (Vert{
+      index: vert_index as usize,
+      uv: uv,
+      weight_ix_start: start_weight as usize,
+      weight_ix_count: weight_count as usize 
+    })
+  )
+);
+
+/// Parse verts section of mesh {}
+named!(parse_md5mesh_mesh_verts<&str, Vec<Vert>>,
+  do_parse!(
+    // Get num verts
+    space >>
+    tag!("numverts") >>
+    space >>
+    num_verts: parse_u32 >>
+    alt!(tag!("\n") | tag!("\r\n") | tag!("\r")) >>
+
+    // Parse all verts
+    verts: count!( parse_md5mesh_mesh_vert, num_verts as usize ) >>
+
+    (verts)
+  )
+);
+
+/// Consumes a line and parses a tri
+named!(parse_md5mesh_mesh_tri<&str, Tri>,
+  do_parse!(
+    opt!(space) >> 
+    tag!("tri") >>
+    space >> 
+    tri_index: parse_u32 >>
+    space >>
+    v1: parse_u32 >>
+    space >>
+    v2: parse_u32 >>
+    space >>
+    v3: parse_u32 >>
+    opt!(space) >>
+    opt!(alt!(tag!("\n") | tag!("\r\n") | tag!("\r"))) >>
+    (Tri{
+      index: tri_index as usize,
+      vert_index_1: v1 as usize,
+      vert_index_2: v2 as usize,
+      vert_index_3: v3 as usize,
+    })
+  )
+);
+
+/// Parse tris section of mesh {}
+named!(parse_md5mesh_mesh_tris<&str, Vec<Tri>>,
+  do_parse!(
+    // Get num tris
+    space >>
+    tag!("numtris") >>
+    space >>
+    num_tris: parse_u32 >>
+    alt!(tag!("\n") | tag!("\r\n") | tag!("\r")) >>
+
+    // Parse all tris
+    tris: count!( parse_md5mesh_mesh_tri, num_tris as usize ) >>
+
+    (tris)
+  )
+);
+
+/// Consumes a line and parses a weight
+named!(parse_md5mesh_mesh_weight<&str, Weight>,
+  do_parse!(
+    opt!(space) >> 
+    tag!("weight") >>
+    space >> 
+    weight_index: parse_u32 >>
+    space >>
+    joint: parse_u32 >>
+    space >>
+    bias: parse_f32 >>
+    space >>
+    pos: delimited!(tag!("("), parse_vec3f, tag!(")")) >>
+    opt!(space) >>
+    opt!(alt!(tag!("\n") | tag!("\r\n") | tag!("\r"))) >>
+    (Weight{
+      index: weight_index as usize,
+      joint: joint as usize,
+      bias: bias,
+      pos: pos,
+    })
+  )
+);
+
+/// Parse weights section of mesh {}
+named!(parse_md5mesh_mesh_weights<&str, Vec<Weight>>,
+  do_parse!(
+    // Get num weights
+    space >>
+    tag!("numweights") >>
+    space >>
+    num_weights: parse_u32 >>
+    alt!(tag!("\n") | tag!("\r\n") | tag!("\r")) >>
+
+    // Parse all weights
+    weights: count!( parse_md5mesh_mesh_weight, num_weights as usize ) >>
+
+    (weights)
+  )
+);
+
+/// Consumes a line, parses the shader name
+named!(parse_md5mesh_mesh_shader<&str, &str>,
+  do_parse!(
+    space >>
+    tag!("shader") >>
+    space >>
+    shader_name: delimited!(tag!("\""), take_until!("\""), tag!("\"")) >>
+    alt!(tag!("\n") | tag!("\r\n") | tag!("\r")) >>
+    (shader_name)
+  )
+);
+
+/// Parse mesh {} section of md5mesh
+named!(parse_md5mesh_mesh<&str, MD5Mesh>,
+  do_parse!(
+    // Parse the start of the block
+    tag!("mesh {") >>
+    alt!(tag!("\n") | tag!("\r\n") | tag!("\r")) >>
+    shader: parse_md5mesh_mesh_shader >>
+    many0!(alt!(tag!("\n") | tag!("\r\n") | tag!("\r"))) >>
+    verts: parse_md5mesh_mesh_verts >>
+    many0!(alt!(tag!("\n") | tag!("\r\n") | tag!("\r"))) >>
+    tris: parse_md5mesh_mesh_tris >>
+    many0!(alt!(tag!("\n") | tag!("\r\n") | tag!("\r"))) >>
+    weights: parse_md5mesh_mesh_weights >>
+    many0!(alt!(tag!("\n") | tag!("\r\n") | tag!("\r"))) >>
+    // Parse end of black
+    tag!("}") >>
+    (MD5Mesh{ verts: verts, tris: tris, weights: weights, shader: shader.to_owned() })
+  )
+);
 
 /// Parser for the whole md5mesh file
 named!(parse_md5mesh<&str, MD5Scene>,
@@ -199,7 +407,15 @@ named!(parse_md5mesh<&str, MD5Scene>,
     header: parse_md5mesh_header >>
     opt!(alt!(tag!("\n") | tag!("\r\n") | tag!("\r"))) >>
     joints: parse_md5mesh_joints >> 
-    (MD5Scene::new(header, joints))
+    opt!(alt!(tag!("\n") | tag!("\r\n") | tag!("\r"))) >>
+    meshes: many0!(
+      do_parse!(
+        mesh: parse_md5mesh_mesh >>
+        many0!(alt!(tag!("\n") | tag!("\r\n") | tag!("\r"))) >>
+        (mesh)
+      )
+    ) >>
+    (MD5Scene::new(header, joints, meshes))
   )
 );
 
